90047c3b1fbd7db464b4286f027c3512a0e18cf2
karthick92 <karthick@sae.bid>
2016-03-20 08:52:21 -0700

    database details

--- ajax-chat/php/ajax.php
+++ ajax-chat/php/ajax.php
@@ -3,10 +3,10 @@
 /* Database Configuration. Add your details below */
 
 $dbOptions = array(
-	'db_host' => '',
-	'db_user' => '',
-	'db_pass' => '',
-	'db_name' => ''
+	'db_host' => 'karsecurity.c0idd0qyk4a7.us-west-2.rds.amazonaws.com:3306',
+	'db_user' => 'karthick92',
+	'db_pass' => 'Karthick92',
+	'db_name' => 'webchat'
 );
 
 /* Database Config End */
